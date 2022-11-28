46dbfda9b4d6bd9b9f23d33f85faf5513fdf51c5
Vlad <vladbondarenko944@gmal.com>
2022-11-28 23:03:13 +0300

    Version own 1.0

---
+++ main.py
@@ -0,0 +1,123 @@
+class Avengers:
+    teams_name = 'Avengers'
+    director = 'Nick Fury'
+
+
+    def __init__ (self, name, nickname, actors_name, traits, weapon, powers, amount_of_films):
+        self.name = name
+        self.nickname = nickname
+        self.actors_name = actors_name
+        self.traits = traits
+        self.weapon = weapon
+        self.powers = powers
+        self.amount_of_films = amount_of_films
+
+
+
+Avenger1 = Avengers(name = 'Steve Rogers', nickname = 'Catain Amerika', actors_name = 'Chris Evans', traits = 'tactical, kind, patrotic', weapon = 'shield', powers = 'none', amount_of_films = 7)
+Avenger2 = Avengers(name = 'Tony Stark', nickname = 'Iron Man', actors_name = 'Robert Downey', traits = 'confident, funny, smart', weapon = 'iron suit', powers = 'none', amount_of_films = 9)
+Avenger3 = Avengers(name = 'Thor Odinson', nickname = 'Thor', actors_name = 'Chirs Hemsworth', traits = 'wise, experienced, skilled', weapon = 'hammer Mjolnir and Stormbreaker', powers = 'lights and thunder', amount_of_films = 8)
+Avenger4 = Avengers(name = 'Brus Benner', nickname = 'Hulk', actors_name = 'Mark Ruffalo', traits = 'smart, kind', weapon = 'Hulk', powers = 'Hulk', amount_of_films = 5)
+Avenger5 = Avengers(name = 'Natash Romanoff', nickname = 'Blak Widow', actors_name = 'Scralett Johansson', traits = 'smart, fast, pretty', weapon = 'shockers', powers = 'none', amount_of_films = 9)
+Avenger6 = Avengers(name = 'Klint Barton', nickname = 'Hawkeye', actors_name = 'Jeremy Renner', traits = 'kind, well-aimed, fast', weapon = 'bow', powers = 'none', amount_of_films = 5)
+
+
+print(' ')
+print(' ')
+print('                  Avengers: ')
+print(' ')
+print(' ')
+
+
+print('Avenger 1: ')
+print(' ')
+
+print('Name:', Avenger1.name)
+print('Nickname:', Avenger1.nickname)
+print('Teams name:', Avengers.teams_name)
+print('Actors name:', Avenger1.actors_name)
+print('Directors name:', Avengers.director)
+print('Traits:', Avenger1.traits)
+print('Weapon:', Avenger1.weapon)
+print('Powers:', Avenger1.powers)
+print('Amount of films:', Avenger1.amount_of_films)
+
+print(' ')
+print(' ')
+
+print('Avenger 2: ')
+print(' ')
+
+print('Name:', Avenger2.name)
+print('Nickname:', Avenger2.nickname)
+print('Teams name:', Avengers.teams_name)
+print('Actors name:', Avenger2.actors_name)
+print('Directors name:', Avengers.director)
+print('Traits:', Avenger2.traits)
+print('Weapon:', Avenger2.weapon)
+print('Powers:', Avenger2.powers)
+print('Amount of films:', Avenger2.amount_of_films)
+
+print(' ')
+print(' ')
+
+print('Avenger 3: ')
+print(' ')
+
+print('Name:', Avenger3.name)
+print('Nickname:', Avenger3.nickname)
+print('Teams name:', Avengers.teams_name)
+print('Actors name:', Avenger3.actors_name)
+print('Directors name:', Avengers.director)
+print('Traits:', Avenger3.traits)
+print('Weapon:', Avenger3.weapon)
+print('Powers:', Avenger3.powers)
+print('Amount of films:', Avenger3.amount_of_films)
+
+print(' ')
+print(' ')
+
+print('Avenger 4: ')
+print(' ')
+
+print('Name:', Avenger4.name)
+print('Nickname:', Avenger4.nickname)
+print('Teams name:', Avengers.teams_name)
+print('Actors name:', Avenger4.actors_name)
+print('Directors name:', Avengers.director)
+print('Traits:', Avenger4.traits)
+print('Weapon:', Avenger4.weapon)
+print('Powers:', Avenger4.powers)
+print('Amount of films:', Avenger4.amount_of_films)
+
+print(' ')
+print(' ')
+
+print('Avenger 5: ')
+print(' ')
+
+print('Name:', Avenger5.name)
+print('Nickname:', Avenger5.nickname)
+print('Teams name:', Avengers.teams_name)
+print('Actors name:', Avenger5.actors_name)
+print('Directors name:', Avengers.director)
+print('Traits:', Avenger5.traits)
+print('Weapon:', Avenger5.weapon)
+print('Powers:', Avenger5.powers)
+print('Amount of films:', Avenger5.amount_of_films)
+
+print(' ')
+print(' ')
+
+print('Avenger 6: ')
+print(' ')
+
+print('Name:', Avenger6.name)
+print('Nickname:', Avenger6.nickname)
+print('Teams name:', Avengers.teams_name)
+print('Actors name:', Avenger6.actors_name)
+print('Directors name:', Avengers.director)
+print('Traits:', Avenger6.traits)
+print('Weapon:', Avenger6.weapon)
+print('Powers:', Avenger6.powers)
+print('Amount of films:', Avenger6.amount_of_films)
